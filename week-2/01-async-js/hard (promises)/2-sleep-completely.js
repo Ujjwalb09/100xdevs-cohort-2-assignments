0e3ea0703f9a81e3c22fa62ff95d1a24d16147fd
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-12-17 21:37:15 +0530

    Done with week 2 medium and hard assignments on promises and async JS

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -4,7 +4,27 @@
  * the function should return a promise just like before
  */
 
-function sleep(milliseconds) {
-}
+//making thread sleep using setTimeout()
 
+// function sleep(milliseconds) {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//         resolve();
+//         },milliseconds)
+//     })
+// }
+
+//making thread sleep using busy wait loop
+
+function sleep(milliseconds){
+    return new Promise((resolve)=>{
+        let startTime = Date.now();
+
+        while(Date.now() - startTime < milliseconds){
+            //Busy-wait loop
+        }
+
+        resolve();
+    })
+}
 module.exports = sleep;
