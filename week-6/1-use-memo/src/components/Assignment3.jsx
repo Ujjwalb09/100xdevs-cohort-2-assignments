0bd7cc6281f1fb20901e1f153a49b8a9e425eaf8
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-03-06 21:58:48 +0530

    week 6 useMemo all assignment completed

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,20 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+        let totalCost = 0;
+
+        items.forEach((item)=>{
+            totalCost += item.value;
+        })
+
+        return totalCost;
+    }, [items]);
     // Your code ends here
     return (
         <div>
